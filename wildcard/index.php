6e034783f5ce6de87e5db929c2657a51f7463b5c
VashuKochar <vashukochar@gmail.com>
2025-04-12 10:13:15 +0000

    Refactor index.php to include header and footer snippets, removing hardcoded navigation and footer content.

--- wildcard/index.php
+++ wildcard/index.php
@@ -8,17 +8,7 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="https://swgaudit.com">Home</a></li>
-                <li><a href="https://phishing.swgaudit.com">Phishing</a></li>
-                <li><a href="https://malware.swgaudit.com">Malware</a></li>
-                <li><a href="https://data-theft.swgaudit.com">Data Theft</a></li>
-                <li><a href="https://cyberslacking.swgaudit.com">Cyber-Slacking</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php include '../snippets/header.php' ?>
     <main>
         <!-- Show the URL in a HTML Page -->
         <p>Your current URL is: <span id="url"></span></p>
@@ -26,11 +16,6 @@
             document.getElementById('url').textContent = window.location.href;
         </script>
     </main>
-    <footer>
-    <p>Configure your SWG to treat <?php
-        $host = $_SERVER['HTTP_HOST']; // Get the host (domain) from the current URL
-        echo $host; // Output the domain from the URL
-    ?> as an uncategorized domain.</p>
-</footer>
-\</body>
+    <?php include '../snippets/footer.php' ?>
+</body>
 </html>
